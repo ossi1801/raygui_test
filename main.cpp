cfde08e7a49d73b77161cd3b4bd1b256a8feb2e9
ossi1801 <ossihakki@gmail.com>
2025-06-08 00:10:38 +0300

    vector

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ const int GAME_WIDTH = 600;
 const int GAME_HEIGHT = 400;
 
 struct {
-  float x;
-  float y;
+
+  Vector2 pos;
   float w;
   float h;
   float speed;
@@ -20,8 +20,8 @@ struct {
 } player;
 
 void init_player() {
-  player.x = 0;
-  player.y = 0;
+  player.pos.x = 0;
+  player.pos.y = 0;
   player.w = 20;
   player.h = 20;
   player.speed = 0.05;
@@ -34,15 +34,17 @@ void init_player() {
 void loop() {
   BeginDrawing();
   ClearBackground(SKYBLUE);
-  Vector2 v1 = {player.x + sinf(player.rotation * DEG2RAD) * (player.h),
-                player.y - cosf(player.rotation * DEG2RAD) * (player.h)};
-  Vector2 v2 = {player.x - cosf(player.rotation * DEG2RAD) * (player.w / 2),
-                player.y - sinf(player.rotation * DEG2RAD) * (player.w / 2)};
-  Vector2 v3 = {player.x + cosf(player.rotation * DEG2RAD) * (player.w / 2),
-                player.y + sinf(player.rotation * DEG2RAD) * (player.w / 2)};
+  Vector2 v1 = {player.pos.x + sinf(player.rotation * DEG2RAD) * (player.h),
+                player.pos.y - cosf(player.rotation * DEG2RAD) * (player.h)};
+  Vector2 v2 = {player.pos.x - cosf(player.rotation * DEG2RAD) * (player.w / 2),
+                player.pos.y -
+                    sinf(player.rotation * DEG2RAD) * (player.w / 2)};
+  Vector2 v3 = {player.pos.x + cosf(player.rotation * DEG2RAD) * (player.w / 2),
+                player.pos.y +
+                    sinf(player.rotation * DEG2RAD) * (player.w / 2)};
 
   DrawTriangle(v1, v2, v3, RED);
-  // DrawRectangle(player.x, player.y, player.w, player.h, RED);
+  // DrawRectangle(player.pos.x, player.pos.y, player.w, player.h, RED);
   DrawText("Hello world", 100, 100, 10, RED);
   //}
 
@@ -50,16 +52,16 @@ void loop() {
 }
 void check_user_input() {
   if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
-    player.x -= player.speed;
+    player.pos.x -= player.speed;
   }
   if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
-    player.x += player.speed;
+    player.pos.x += player.speed;
   }
   if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
-    player.y += player.speed;
+    player.pos.y += player.speed;
   }
   if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-    player.y -= player.speed;
+    player.pos.y -= player.speed;
   }
 }
 
@@ -67,17 +69,17 @@ void collisions() {
   // TODO: use
   // CheckCollisionRecs(Rectangle rec1, Rectangle rec2)
 
-  if (player.x <= 0) {
-    player.x = 0;
+  if (player.pos.x <= 0) {
+    player.pos.x = 0;
   }
-  if (player.x >= GAME_WIDTH - player.w) {
-    player.x = GAME_WIDTH - player.w;
+  if (player.pos.x >= GAME_WIDTH - player.w) {
+    player.pos.x = GAME_WIDTH - player.w;
   }
-  if (player.y <= 0) {
-    player.y = 0;
+  if (player.pos.y <= 0) {
+    player.pos.y = 0;
   }
-  if (player.y >= GAME_HEIGHT - player.h) {
-    player.y = GAME_HEIGHT - player.h;
+  if (player.pos.y >= GAME_HEIGHT - player.h) {
+    player.pos.y = GAME_HEIGHT - player.h;
   }
 }
 int main(int argc, char *argv[]) {
